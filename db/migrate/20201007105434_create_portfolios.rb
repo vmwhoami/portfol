bdc951e50f4f25e3b6f900af92a2ef26e23ca92a
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-07 13:57:52 +0300

    Add Portfolios resource

---
+++ db/migrate/20201007105434_create_portfolios.rb
@@ -0,0 +1,13 @@
+class CreatePortfolios < ActiveRecord::Migration[6.0]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_img
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_094839) do
+ActiveRecord::Schema.define(version: 2020_10_07_105434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,23 @@ ActiveRecord::Schema.define(version: 2020_10_07_094839) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_img"
+    t.text "thumb_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.integer "percentage_completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
