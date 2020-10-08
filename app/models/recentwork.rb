82b3347309b1e73d06528fc9f9948f3d9da19563
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-08 13:54:11 +0300

    Add custom scope functionality

--- app/models/recentwork.rb
+++ app/models/recentwork.rb
@@ -1,3 +1,9 @@
 class Recentwork < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+def self.rails
+  where(subtitle: "Ruby on Rails")
+end
+  # scope :rails, -> { where(subtitle: "Html and CSS")}
+ 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,20 @@
+
+3.times do |t|
+Topic.create!(
+title: "Topic #{t}"
+)
+end
+puts "Topics created"
+
+
 10.times do |post|
   Blog.create!(
     title: "This is the title #{post}",
     body:"  Dragée ice cream sweet cake jelly-o ice cream pie fruitcake.
      Powder sweet roll sugar plum liquorice danish chocolate cake. Ice cream tootsie roll 
      sugar plum sesame snaps. Tootsie roll lollipop chocolate bar chocolate cake cake 
-      cake chupa chups biscuit. Candy canes jelly beans pudding tart powder. Candy canes sweet roll tart "
+      cake chupa chups biscuit. Candy canes jelly beans pudding tart powder. Candy canes sweet roll tart ",
+      topic: Topic.last
     )
 end
 
@@ -21,15 +31,26 @@ puts "Skills created"
 
 
 
-9.times do |p|
+8.times do |p|
   Recentwork.create!(
     title:"Portfolio #{p}",
-    subtitle: "Rails is new norm github was built on it",
+    subtitle: "Html and CSS",
     body: "Caramels dragée tart pie. Halvah donut marshmallow muffin pie tootsie roll oat cake. Jelly beans wafer topping. Cheesecake gummies biscuit biscuit sweet. Wafer topping cupcake danish tiramisu macaroon carrot cake caramels. Pudding brownie jujubes chocolate donut cookie jelly beans oat cake. Fruitcake fruitcake chocolate cake dessert jelly-o biscuit. Marzipan sweet lemon drops candy canes wafer croissant cookie tiramisu. Halvah gummies candy canes marshmallow tart cupcake caramels. Gummies cotton candy cupcake powder jelly beans brownie dragée chocolate candy. Cupcake gummi bears pastry sesame snaps. Jelly beans macaroon fruitcake cookie. Sweet dessert danish sugar plum tootsie roll.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x350"
   )
   end
 
+
+  
+1.times do |p|
+  Recentwork.create!(
+    title:"Portfolio #{p}",
+    subtitle: "Ruby on Rails",
+    body: "Caramels dragée tart pie. Halvah donut marshmallow muffin pie tootsie roll oat cake. Jelly beans wafer topping. Cheesecake gummies biscuit biscuit sweet. Wafer topping cupcake danish tiramisu macaroon carrot cake caramels. Pudding brownie jujubes chocolate donut cookie jelly beans oat cake. Fruitcake fruitcake chocolate cake dessert jelly-o biscuit. Marzipan sweet lemon drops candy canes wafer croissant cookie tiramisu. Halvah gummies candy canes marshmallow tart cupcake caramels. Gummies cotton candy cupcake powder jelly beans brownie dragée chocolate candy. Cupcake gummi bears pastry sesame snaps. Jelly beans macaroon fruitcake cookie. Sweet dessert danish sugar plum tootsie roll.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x350"
+  )
+  end
   
 puts "Recentwork created successfully"
