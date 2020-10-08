f4cf1536680f11aeacc862bcad5b6d71a9953aa7
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-08 11:10:14 +0300

    Implement edit and update functionality

--- app/controllers/recentworks_controller.rb
+++ app/controllers/recentworks_controller.rb
@@ -13,9 +13,27 @@ class RecentworksController < ApplicationController
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to recentworks_path, notice: 'Portfolio item was successfully created' }
-        format.json { render :show, status: :created, location: @blog }
+        format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+def edit
+  @portfolio_item = Recentwork.find(params[:id])
+end
+
+  def update
+    @portfolio_item = Recentwork.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(permited_params)
+        format.html { redirect_to recentworks_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
