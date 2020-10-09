09cff2a2c2b53840020ddcae56de6371b0029b7f
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-09 17:02:18 +0300

    Completed authentication with virtual attr

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,13 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+     before_action :devise_permitted_params, if: :devise_controller?
+   end
+
+  def devise_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update,keys:[:name])
+  end
+  
+end
