5f077016d9c6ff6bd8d703a55ef4b138e2de695f
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-09 13:30:47 +0300

    Set defaults from the model file

--- app/models/recentwork.rb
+++ app/models/recentwork.rb
@@ -5,5 +5,11 @@ def self.rails
   where(subtitle: "Ruby on Rails")
 end
   # scope :rails, -> { where(subtitle: "Html and CSS")}
- 
+  after_initialize :set_defaults
+ def set_defaults
+  self.main_image ||= "https://via.placeholder.com/600x400"
+  self.thumb_image ||= "https://via.placeholder.com/350x350"   
+ end
+
+
 end
