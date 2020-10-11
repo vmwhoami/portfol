6f1a11c5495cab119095e15414e78cfbfef96cbc
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-11 16:20:25 +0300

    add views blog layout and shared helper methods

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -2,7 +2,8 @@ module SharedHelper
 
   def visitor_from
     if session[:where_from] 
-     "<p>Thank you for visiting from #{session[:where_from]}</p>".html_safe
+      content_tag(:p, "Thank you for visiting from #{session[:where_from]}",class: "where_from" )
+    #  "<p>Thank you for visiting from #{session[:where_from]}</p>".html_safe
     end  
   end
 
@@ -10,7 +11,7 @@ module SharedHelper
     if current_user.is_a?(User)  
       link_to  "Logout", destroy_user_session_path, method: :delete  
      else  
-      (link_to  "Register", new_user_session_path ) && (link_to  "Login", new_user_session_path)
+      (link_to  "Register", new_user_session_path ) + (link_to  "Login", new_user_session_path)
    
     end  
   end
