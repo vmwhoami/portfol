33b1a438ec440fb56bdc4b404405d2cc0d762e0c
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-13 13:02:23 +0300

    Add copyright module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,18 @@ class ApplicationController < ActionController::Base
   include SessionSourceConcern
  ## Implementation of Null object pattern
   include  GuestUserConcern
+
+ before_action :set_copyright
+ def set_copyright
+   @copyright = VmwhoamiCopyright::Renderer.copyright 'Vitalie Melnic', 'All rights reserved'
+ end
+  
+end
+
+module VmwhoamiCopyright
+  class Renderer
+    def self.copyright(name,msg)
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
