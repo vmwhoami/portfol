3fce7daa449c2b7b4d620e6e40c491ac26232fae
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-15 17:55:13 +0300

    Style the blog section

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  validates_presence_of :name
-
+ has_many :blogs 
  def first_name
   self.name.split.first
  end
---
+++ db/migrate/20201015134954_add_column_to_blogs.rb
@@ -0,0 +1,5 @@
+class AddColumnToBlogs < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :blogs, :user, foreign_key: true
+  end
+end
