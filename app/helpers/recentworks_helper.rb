5ef03715870c4b4fc3006cdf1e3c885575230e0f
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-09 16:55:38 +0200

    Add overflow hidden to portfolio items image

--- app/helpers/recentworks_helper.rb
+++ app/helpers/recentworks_helper.rb
@@ -1,6 +1,6 @@
 module RecentworksHelper
 
   def add_portfolio_items
-  link_to  "Create new portfolio item", new_recentwork_path  if logged_in?(:site_admin) 
+   link_to  "Create new portfolio item >", new_recentwork_path,class: "new_item"  if logged_in?(:site_admin) 
   end
 end
