703ff54557c81d1e961f8b5ee6c7c57c5441c773
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-09 16:43:31 +0200

    Solve the portfolio desplaying full width

--- app/helpers/recentworks_helper.rb
+++ app/helpers/recentworks_helper.rb
@@ -1,4 +1,6 @@
-# frozen_string_literal: true
-
 module RecentworksHelper
+
+  def add_portfolio_items
+  link_to  "Create new portfolio item", new_recentwork_path  if logged_in?(:site_admin) 
+  end
 end
