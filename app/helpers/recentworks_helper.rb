1ce23f36dc9e87d7f68d5b15156da3c24c623658
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-10 13:21:15 +0200

    add webvisor counter and edit button

--- app/helpers/recentworks_helper.rb
+++ app/helpers/recentworks_helper.rb
@@ -3,5 +3,12 @@ module RecentworksHelper
   def add_portfolio_items
    link_to  "Create new portfolio item >", new_recentwork_path,class: "new_item"  if logged_in?(:site_admin) 
   end
+
+  def edit_item(item)
+    link_to  "Edit", edit_recentwork_path(item), class: "links__container mt-5 links__text"  if logged_in?(:site_admin) 
+  end
  
+  def delete_item(item)
+  end
+
 end
